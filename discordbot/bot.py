58b0b68c9f7a5c4851eaad0e8a9b103e8d45f9ed
h3lls <savagesoul@gmail.com>
2019-12-09 18:20:28 -0800

    Added !who command to get who list from discord, added connection message for mud users

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -16,6 +16,9 @@ mud_password = os.getenv('MUD_PASSWORD')
 mud_host = os.getenv('MUD_HOST')
 mud_port = int(os.getenv('MUD_PORT'))
 
+logged_in = False
+getting_who = False
+
 #for passing messages from mud -> discord
 message_queue = asyncio.Queue()
 
@@ -24,7 +27,11 @@ client = discord.Client()
 def telnet(client):
     pool = concurrent.futures.ThreadPoolExecutor()
     channel = None
+<<<<<<< Updated upstream
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+=======
+    global getting_who, logged_in
+>>>>>>> Stashed changes
     s.settimeout(2)
 
     # connect to remote host
@@ -41,7 +48,7 @@ def telnet(client):
         
         # Get the list sockets which are readable
         read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-        
+        who_text = ""
         for sock in read_sockets:
             #incoming message from remote server
             if sock == s:
@@ -51,26 +58,74 @@ def telnet(client):
                     sys.exit()
                 else:
                     data = data.decode('utf-8', 'backslashreplace')
-                    # This is clearly unsafe.. I know this... it's fine... just fine
-                    if "your handle" in data:
+                    #print(data.strip())
+                    if getting_who:
+                        print("Getting who in loop")
+                        print(data.strip())
+                        who_text = data.strip()
+                        if "displayed." in data:
+                            message_queue.put_nowait("```" + who_text + "```")
+                            who_text = ""
+                            getting_who = False
+                            continue
+                    if "(OOC)" in data:
+                        data = data.strip()
+                        user = data[1:].split(']')[0]
+                        if user.lower() != mud_username.lower():
+                            send_text = "**" + user + "**: " + data[data.find('"')+1:-1]
+                            print("sending to discord: " + send_text)
+                            message_queue.put_nowait(send_text)
+                    # [CONNLOG: [60500] Radiotower [ip72-199-36-114.sd.sd.cox.net] has connected.]
+                    elif "CONNLOG" in data and "has connected" in data:
+                        data = data.strip()
+                        user = data.split(']')[1].split('[')[0][1:-1]
+                        send_text = "**" + user + "** has connected"
+                        print("Connection: " + send_text)
+                        message_queue.put_nowait(send_text)
+                    elif not logged_in and "your handle" in data:
                         print("your handle")
                         s.send((mud_username + "\n").encode())
-                    elif "Welcome back. Enter your password" in data:
+                    elif not logged_in and "Welcome back. Enter your password" in data:
                         s.send((mud_password + "\n").encode())
+                        logged_in = True
                     elif "PRESS RETURN" in data:
                         s.send("\n".encode())
                     elif "Enter the game" in data:
                         s.send("1\n".encode())
+<<<<<<< Updated upstream
                     elif "(OOC)" in data:
                         data = data.strip()
                         send_text = "**" + data[1:].split(']')[0] + "**: " + data[data.find('"')+1:-1]
                         print("sending: " + send_text)
                         message_queue.put_nowait(send_text)
+=======
+>>>>>>> Stashed changes
             else :
                 msg = sys.stdin.readline()
                 s.send(msg.encode())
 
 @client.event
+<<<<<<< Updated upstream
+=======
+async def on_message(message):
+    global getting_who
+    if message.author == client.user:
+        return
+    if message.channel.id != discord_channel:
+        return
+    if "!who" in message.content:
+        getting_who = True
+        print("Getting who list")
+        s.send("who\n".encode())
+        return
+    send_text = "ooc Discord: " + message.author.display_name + " says " + message.content + "\n"
+    print("sending to mud: " + send_text)
+    s.send(send_text.encode())
+    # message.content
+    # message.author.display_name
+
+@client.event
+>>>>>>> Stashed changes
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     channel = client.get_channel(discord_channel)
